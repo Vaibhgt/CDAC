fc952fb80c1f53e3edfa8382e125ba6d4dbc400c
Vaibhgt <101324816+Vaibhgt@users.noreply.github.com>
2022-06-05 15:10:05 +0530

    Add files via upload

---
+++ Day2/Febionacci.java
@@ -0,0 +1,21 @@
+class FebionacciDemo{
+	public static void main (String args[]){
+		int a=0;
+		int b=1;
+		int c;
+		System.out.print(a+" "+b);
+		for (int i=0;i<=10;i++){
+			
+			c=a+b;
+			a=b;
+			b=c;
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ Day2/ReursionDemo.java
@@ -0,0 +1,25 @@
+class RecursionDemo{
+	
+	
+	
+	public static void main (String args[]){
+		int number=5;
+		RecursionDemo f=new RecursionDemo();
+		f.fact(number);
+		
+		
+	}
+	
+
+	
+	void fact(int number){
+		
+		f.fact();
+	}
+	
+	
+		
+		
+	
+	
+}
